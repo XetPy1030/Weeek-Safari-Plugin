30daea9e57518c3205a1706542fd6fda72d6f2fb
XetPy1030 <xetpy1030@mail.ru>
2025-09-09 16:19:59 +0300

    Initial Commit

---
+++ Weeek/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Weeek
+//
+//  Created by MacBook on 09.09.2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
